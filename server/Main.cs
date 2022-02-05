598fc2fb27709ae84e2546e77902394d27b27a19
Abel Gaming <lcpdpolice4939@gmail.com>
2022-02-05 00:49:56 -0500

    Add files via upload

--- server/Main.cs
+++ server/Main.cs
@@ -1,5 +1,7 @@
-using CitizenFX.Core;
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
 using System;
+using System.Collections.Generic;
 
 namespace server
 {
@@ -7,7 +9,18 @@ namespace server
     {
         public Main()
         {
+            Load.Config();
             EventHandlers["ReviveNPC:TriggerServerRevive"] += new Action<Player, int>(RevivePedServer);
+        }
+
+        public static void SendCommand(string command)
+        {
+            RegisterCommand(command, new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                PlayerList pl = new PlayerList();
+                Player player = pl[source];
+                player.TriggerEvent("ReviveNPC:DoCommand");
+            }), false);
         }
 
         private void RevivePedServer([FromSource] Player player, int Ped)
